dcfec89241a796a97869155187f3e7f9fae5e948
mugoalvin <alvin.mugo@student.moringaschool.com>
2022-12-06 21:13:44 +0300

    Added a branch

--- _config.js
+++ _config.js
@@ -2,8 +2,8 @@ var config = {}
 
 // Update to have your correct username and password
 config.mongoURI = {
-    production: 'mongodb+srv://<Alvin Mugo>:<nB9bpLW3D6MSGe9>@myfirstcluster.a8mmcvb.mongodb.net/?retryWrites=true&w=majority',
-    development: 'mongodb+srv://<Alvin Mugo>:<nB9bpLW3D6MSGe9>@myfirstcluster.a8mmcvb.mongodb.net/?retryWrites=true&w=majority',
-    test: 'mongodb+srv://<Alvin Mugo>:<nB9bpLW3D6MSGe9>@myfirstcluster.a8mmcvb.mongodb.net/?retryWrites=true&w=majority',
+    production: 'mongodb+srv://Atlas_Cluster:y1bLoBHoyF96qomg@myfirstcluster.a8mmcvb.mongodb.net/?retryWrites=true&w=majority',
+    development: 'mongodb+srv://Atlas_Cluster:y1bLoBHoyF96qomg@myfirstcluster.a8mmcvb.mongodb.net/?retryWrites=true&w=majority',
+    test: 'mongodb+srv://Atlas_Cluster:y1bLoBHoyF96qomg@myfirstcluster.a8mmcvb.mongodb.net/?retryWrites=true&w=majority',
 }
 module.exports = config;
